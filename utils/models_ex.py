9eeeee75e75adc80bfcbd5a0732b8686de20ef8f
ccwow123 <1849325557@qq.com>
2023-03-21 19:35:21 +0800

    Ver1.5 加入unet作为生成器

---
+++ utils/models_ex.py
@@ -0,0 +1,132 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class BaseBlock(nn.Module):
+    def __init__(self, input_nc,output_nc,mid_channels=None,stride=1):
+        super().__init__()
+        if mid_channels is None:
+            mid_channels = output_nc
+        conv_block = [  nn.Conv2d(input_nc, mid_channels, 3, stride=stride, padding=1),
+                    nn.InstanceNorm2d(mid_channels),
+                    nn.LeakyReLU(0.2, inplace=True)  ]
+
+        self.conv_block = nn.Sequential(*conv_block)
+
+    def forward(self, x):
+        return self.conv_block(x)
+
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super(Up, self).__init__()
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = BaseBlock(in_channels, out_channels)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = BaseBlock(in_channels, out_channels)
+
+    def forward(self, x1: torch.Tensor, x2: torch.Tensor) -> torch.Tensor:
+        x1 = self.up(x1)
+        # x = torch.cat([x2, x1], dim=1)
+        x = x1 + x2
+        x = self.conv(x)
+        return x
+class Generator(nn.Module):
+    def __init__(self, input_nc, output_nc, bilinear=True):
+        super(Generator, self).__init__()
+        #           0, 1, 2, 3, 4
+        channles = [32,64,128,256,512]
+        self.con_1 = BaseBlock(input_nc,channles[0],stride=1)
+        self.con_2 = BaseBlock(channles[0],channles[0],stride=2)
+        self.con_3 = BaseBlock(channles[0],channles[1],stride=1)
+        self.con_4 = BaseBlock(channles[1],channles[1],stride=2)
+        self.con_5 = BaseBlock(channles[1],channles[2],stride=1)
+        self.con_6 = BaseBlock(channles[2],channles[2],stride=2)
+        self.con_7 = BaseBlock(channles[2],channles[3],stride=1)
+        self.con_8 = BaseBlock(channles[3],channles[3],stride=2)
+        self.con_9 = BaseBlock(channles[3],channles[4],stride=1)
+        self.con_10 = BaseBlock(channles[4],channles[4],stride=2) #bottleneck
+
+
+        self.up_9 = Up(channles[4],channles[3])
+        self.decon_8 = BaseBlock(channles[3],channles[3],stride=1)
+        self.up7 = Up(channles[3],channles[2])
+        self.decon_6 = BaseBlock(channles[2],channles[2],stride=1)
+        self.up5 = Up(channles[2],channles[1])
+        self.decon_4 = BaseBlock(channles[1],channles[1],stride=1)
+        self.up3 = Up(channles[1],channles[0])
+        self.decon_2 = BaseBlock(channles[0],channles[0],stride=1)
+        self.up1 = Up(channles[0],channles[0])
+
+
+        self.out = nn.Conv2d(channles[0], output_nc, kernel_size=1, stride=1, padding=0)
+
+    def forward(self, x):
+        x1 = self.con_1(x)
+        x2 = self.con_2(x1)
+        x3 = self.con_3(x2)
+        x4 = self.con_4(x3)
+        x5 = self.con_5(x4)
+        x6 = self.con_6(x5)
+        x7 = self.con_7(x6)
+        x8 = self.con_8(x7)
+        x9 = self.con_9(x8)
+        x10 = self.con_10(x9)
+
+        x9_2 = self.up_9(x10,x9)
+        x8_2 = self.decon_8(x9_2)
+        x7_2 = self.up7(x8_2,x7)
+        x6_2 = self.decon_6(x7_2)
+        x5_2 = self.up5(x6_2,x5)
+        x4_2 = self.decon_4(x5_2)
+        x3_2 = self.up3(x4_2,x3)
+        x2_2 = self.decon_2(x3_2)
+        x1_2 = self.up1(x2_2,x1)
+        x = self.out(x1_2)
+
+
+        return x
+
+
+class Discriminator(nn.Module):
+    def __init__(self, input_nc):
+        super(Discriminator, self).__init__()
+
+        # A bunch of convolutions one after another
+        model = [   nn.Conv2d(input_nc, 64, 4, stride=2, padding=1),
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        model += [  nn.Conv2d(64, 128, 4, stride=2, padding=1),
+                    nn.InstanceNorm2d(128), 
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        model += [  nn.Conv2d(128, 256, 4, stride=2, padding=1),
+                    nn.InstanceNorm2d(256), 
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        model += [  nn.Conv2d(256, 512, 4, padding=1),
+                    nn.InstanceNorm2d(512), 
+                    nn.LeakyReLU(0.2, inplace=True) ]
+
+        # FCN classification layer
+        model += [nn.Conv2d(512, 1, 4, padding=1)]
+
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        x =  self.model(x)
+        # Average pooling and flatten
+        return F.avg_pool2d(x, x.size()[2:]).view(x.size()[0], -1)
+
+if __name__ == '__main__':
+    # Test the model
+    x = torch.randn((1, 3, 512, 512))
+    G = Generator(3, 1)
+    y = G(x)
+    print('y.shape:' , y.shape)
+
+    # D = Discriminator(3)
+    # out = D(y)
+    # print(out.shape)
