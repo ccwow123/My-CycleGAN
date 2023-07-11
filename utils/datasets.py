b276cb555d63ef2daab1baf6da048f56a316b229
ccwow <1849325557@qq.com>
2023-07-11 16:09:29 +0800

    Ver1.7 加入pix2pix

--- utils/datasets.py
+++ utils/datasets.py
@@ -26,3 +26,45 @@ class ImageDataset(Dataset):
 
     def __len__(self):
         return max(len(self.files_A), len(self.files_B))
+
+
+
+def to_rgb(image):
+    rgb_image = Image.new("RGB", image.size)
+    rgb_image.paste(image)
+    return rgb_image
+
+
+class ImageDataset_pix2pix(Dataset):
+    def __init__(self, root, transforms_=None, mode="train"):
+        self.transform = transforms.Compose(transforms_)
+        if mode == "train":
+            self.files_A = sorted(glob.glob(os.path.join(root, "train/A") + "/*.*"))
+            self.files_B = sorted(glob.glob(os.path.join(root, "train/B") + "/*.*"))
+        else:
+            self.files_A = sorted(glob.glob(os.path.join(root, "test/A") + "/*.*"))
+            self.files_B = sorted(glob.glob(os.path.join(root, "test/B") + "/*.*"))
+
+        # self.files_A = sorted(glob.glob(os.path.join(root, "%s/A" % mode) + "/*.*"))
+        # self.files_B = sorted(glob.glob(os.path.join(root, "%s/B" % mode) + "/*.*"))
+        # 确保输入文件夹和目标文件夹中图像文件数量一致
+        assert len(self.files_A) == len(self.files_B), "Number of input and target images doesn't match."
+
+    def __getitem__(self, index):
+        image_A = Image.open(self.files_A[index]) # 从文件夹中读取图像
+
+        image_B = Image.open(self.files_B[index])
+
+        # Convert grayscale images to rgb
+        if image_A.mode != "RGB":
+            image_A = to_rgb(image_A)
+        if image_B.mode != "RGB":
+            image_B = to_rgb(image_B)
+
+        item_A = self.transform(image_A)
+        item_B = self.transform(image_B)
+        return {"A": item_A, "B": item_B}
+
+    def __len__(self):
+        return max(len(self.files_A), len(self.files_B))
+
